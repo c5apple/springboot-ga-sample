bbcafeb08b45f80b552edb11509dbf89d7f7c9e2
c5apple <c5apple_party@yahoo.co.jp>
2019-11-29 20:37:53 +0900

    GoogleAnalytics処理

--- src/main/java/com/example/demo/config/GoogleAnalyticsConfig.java
+++ src/main/java/com/example/demo/config/GoogleAnalyticsConfig.java
@@ -1,13 +1,39 @@
 package com.example.demo.config;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.analytics.Analytics;
+import com.google.api.services.analytics.AnalyticsScopes;
 
 @Configuration
 public class GoogleAnalyticsConfig {
 
 	@Bean
-	public void analytics() {
-		// TODO
+	public Analytics analytics() throws GeneralSecurityException, IOException {
+		String applicationName = "googleAnalytics";
+		JacksonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+		String serviceAccountEmail = "xxx@gserviceaccount.com";
+		String keyFileLocation = "/credential.p12";
+
+		NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+		GoogleCredential credential = new GoogleCredential.Builder()
+				.setTransport(httpTransport)
+				.setJsonFactory(jsonFactory)
+				.setServiceAccountId(serviceAccountEmail)
+				.setServiceAccountPrivateKeyFromP12File(
+						new ClassPathResource(keyFileLocation).getFile())
+				.setServiceAccountScopes(AnalyticsScopes.all())
+				.build();
+		return new Analytics.Builder(httpTransport, jsonFactory, credential)
+				.setApplicationName(applicationName).build();
 	}
 }
