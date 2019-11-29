b5e6198b28ac0de839072f7f025e01bf6ba29631
c5apple <c5apple_party@yahoo.co.jp>
2019-11-29 17:35:01 +0900

    hello world

---
+++ src/main/java/com/example/demo/config/GoogleAnalyticsConfig.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GoogleAnalyticsConfig {
+
+	@Bean
+	public void analytics() {
+		// TODO
+	}
+}
